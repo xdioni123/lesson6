19d11903d375a95841e76d4bb6cea9d1a6698433
xdioni123 <dion.ibrahimi2008@gmail.com>
2025-03-27 20:47:17 +0100

    set lesson6

--- test.py
+++ test.py
@@ -1,5 +1,8 @@
 # from mymodule import greet
 import mymodule
+# from math import sqrt, pi
+from math import sqrt  as sq
+from my_package import greet
 
 print(mymodule.greet("Filan"))
 
@@ -7,3 +10,9 @@ cal = mymodule.Calculator()
 
 print(cal.add(5, 3))
 print(cal.substrack(10, 4))
+
+# result = math.sqrt(25)
+# print("Square root of 36 is:", sqrt(36))
+# print("Value of pi is:", pi)
+print("Square root of 36 is:", sq(36))
+print(greet("Filan2"))
