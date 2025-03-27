304eb7600e6854e2acb59789028db99ed4bca650
xdioni123 <dion.ibrahimi2008@gmail.com>
2025-03-27 20:00:10 +0100

    set - lesso6

--- mymodule.py
+++ mymodule.py
@@ -2,3 +2,10 @@
 
 def greet(name):
     return f"Hello, {name}"
+
+class Calculator:
+    def add(self, a, b):
+        return a + b
+    
+    def substrack(self, a, b):
+        return a - b
--- test.py
+++ test.py
@@ -1,3 +1,9 @@
+# from mymodule import greet
 import mymodule
 
 print(mymodule.greet("Filan"))
+
+cal = mymodule.Calculator()
+
+print(cal.add(5, 3))
+print(cal.substrack(10, 4))
